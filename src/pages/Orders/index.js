8c663e3f196f0843ea152dc9ebae89764b3cf977
Abhijithlal V <abhilal535@gmail.com>
2022-06-13 20:57:43 +0530

    Made changes

--- src/pages/Orders/index.js
+++ src/pages/Orders/index.js
@@ -1,7 +1,7 @@
 import { checkAuth, withNavbar, withSidebar } from "../../components";
 
 const Orders = () => {
-  return <>Orders</>;
+  return <>Orders-This change is made by abhijithlal v</>;
 };
 
 export default checkAuth(withSidebar(withNavbar(Orders)));
