deb177a85b92407ad964c173dcf7c7801d8f2816
Jerry <jerrythomasjohn9@gmail.com>
2022-06-27 21:54:52 +0530

    Orders api integration started

--- src/components/Order/index.js
+++ src/components/Order/index.js
@@ -1,57 +1,61 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Order = () => {
+const Order = ({ data }) => {
   return (
     <div className="bg-green mt-12 w-11/12 lg:w-9/12  lg:h-80 mx-auto lg:mx-0 lg:ml-20 shadow-xl">
-      <div className="bg-[#E0C7C7] h-1/5 flex flex-row pt-1 hidden lg:flex">
+      <div className="bg-[#E0C7C7] h-1/5 flex-row pt-1 hidden lg:flex">
         <div className="flex w-4/6 justify-left gap-32 ml-8">
           <div className="text-center text-black">
-            <p>ORDER PLACED</p>
-            <p>8June 2022</p>
+            <p>Order placed</p>
+            <p className="font-semibold">
+              {new Date(data.createdAt).toDateString()}
+            </p>
           </div>
 
           <div className="text-center text-black">
-            <p>SHIP TO</p>
-            <p>Trivandrum</p>
+            <p>Ship to</p>
+            <p className="font-semibold">Trivandrum</p>
           </div>
         </div>
-        <div className="w-2/6 flex justify-end mr-12">
-          <div className="text-left align-right">
-            <div className="flex flex-row gap-2 text-black">
-              <p>ORDER</p>
-              <p>#406-8182131-5245131</p>
-            </div>
-
-            <p className="text-[#2838C1] ">View order details</p>
+        <div className="w-2/6 flex justify-end mr-4">
+          <div className="flex flex-row gap-2 text-black items-center">
+            Order ID:<p className="font-semibold">#{data._id}</p>
           </div>
         </div>
       </div>
 
       <div className="bg-[#FFFFFF] h-fit lg:h-4/5 lg:flex flex-row">
         <div className="h-full lg:w-3/12 items-center lg:pl-4">
-          <div className="bg-gray h-[180px]  lg:w-[226px]  my-4   mt-6">
-            <img
-              src="https://res.cloudinary.com/nithionite/image/upload/v1652549936/tt1vkbj7e88prgztmdmk.jpg"
-              className="h-full w-full items-center"
-            />
-          </div>
-
-          <div className="">
-            <p className="pt-2 text-left text-xl font-bold text-black hidden lg:flex">
-              Archive Order
-            </p>
+          <div className="bg-gray h-[180px] lg:w-[226px] my-4 mt-6 carousel carousel-vertical rounded-box scrollbar-default">
+            {data.items.map((item) => (
+              <div
+                className="carousel-item h-full justify-center relative"
+                key={item.product_id + "img"}
+              >
+                <img src={item.image} className="z-10" />
+                <img
+                  src={item.image}
+                  className="h-full w-full absolute top-0 left-0 blur-md"
+                />
+              </div>
+            ))}
           </div>
         </div>
         <div className="h-full pt-5 lg:w-6/12 text-black lg:flex flex-col lg:justify-between">
           <div className="text-center lg:text-left">
-            <p>
-              Fadman Hot Melt Electric Heating Glue Stick Flexible for DIY,
-              Sealing and Quick Repairs (Transparent, 11X177mm, 20 Sticks)
-            </p>
+            {data.items.map((item, index) => {
+              return (
+                <p key={item.product_id + index}>
+                  {item.description}
+                  {index !== data.items.length - 1 && ","}
+                </p>
+              );
+            })}
           </div>
 
           <div className="text-center text-black lg:text-right lg:text-2xl font-bold pt-8 lg:pt-0 lg:mb-4">
-            Total: $299.0
+            Total: Rs.{data.price}
           </div>
 
           <div className="mt-4 lg:mt-0  lg:mb-4 text-center">
@@ -88,26 +92,14 @@ const Order = () => {
         <div className="h-full lg:w-3/12 justify-cente ">
           <div className=" justify-center lg:mx-0 mt-10 lg:mt-0 pb-4 lg:pb-0 pl-4 lg:pl-12 lg:pt-6 space-x-6 lg:space-x-0   lg:space-y-6 flex flex-row lg:block ">
             <div>
-              <button className="lg:h-[36px] px-7 lg:px-0 lg:w-11/12 text-black  bg-[#EF8D33]  hover:bg-[#EF8D33]  rounded-3xl font-semibold text-white py-1 px-4   rounded-xl">
+              <button className="lg:h-[36px] lg:px-0 lg:w-11/12  bg-[#EF8D33]  hover:bg-[#EF8D33] font-semibold text-[#fff] py-1 px-4   rounded-xl">
                 Track Package
               </button>
             </div>
 
             <div>
-              <button className="lg:h-[36px] lg:w-11/12 text-black bg-transparent  hover:bg-[#f6f5f5] text-blue-700 rounded-3xl font-semibold  py-1 px-4 border border-[#d5d9d9]  rounded-xl">
+              <button className="lg:h-[36px] lg:w-11/12 text-black bg-transparent  hover:bg-[#f6f5f5] text-blue-700 font-semibold  py-1 px-4 border border-[#d5d9d9]  rounded-xl">
                 Request Cancellation
-              </button>
-            </div>
-
-            <div className="justify-center ">
-              <button className="h-[36px] w-11/12 text-black bg-transparent  hover:bg-[#f6f5f5] text-blue-700 rounded-3xl font-semibold  py-1 px-4 border border-[#d5d9d9]  rounded-xl hidden lg:block">
-                Leave seller feedback
-              </button>
-            </div>
-
-            <div>
-              <button className="h-[36px] w-11/12 text-black bg-transparent  hover:bg-[#f6f5f5] text-blue-700 rounded-3xl font-semibold  py-1 px-4 border border-[#d5d9d9]  rounded-xl hidden lg:block">
-                Write a product review
               </button>
             </div>
           </div>
@@ -115,6 +107,10 @@ const Order = () => {
       </div>
     </div>
   );
+};
+
+Order.propTypes = {
+  data: PropTypes.object,
 };
 
 export default Order;
--- src/pages/Orders/index.js
+++ src/pages/Orders/index.js
@@ -1,15 +1,32 @@
-import { checkAuth, withNavbar, withSidebar, Order } from "../../components";
+import {
+  checkAuth,
+  withNavbar,
+  withSidebar,
+  Order,
+  CustomLoader,
+} from "../../components";
 import { useState, useEffect } from "react";
 import { getOrders } from "../../api/orders/get";
 import { getApiError } from "../../helpers/getApiError";
+import { useNavigate } from "react-router-dom";
 
 const Orders = () => {
+  const navigate = useNavigate();
   const [category, setCategory] = useState("");
+  const [loading, isLoading] = useState(false);
   const [orders, setOrders] = useState([]);
   useEffect(() => {
+    isLoading(true);
     getOrders()
-      .then((response) => setOrders(response.data))
-      .catch((error) => getApiError(error));
+      .then((response) => {
+        console.log(response.data);
+        setOrders(response.data);
+        isLoading(false);
+      })
+      .catch((error) => {
+        getApiError(error);
+        isLoading(false);
+      });
   }, [category]);
 
   const handleCategoryChange = (category) => {
@@ -17,31 +34,52 @@ const Orders = () => {
   };
   return (
     <>
-      <div className=" mt-14  h-auto mx-2">
-        <div className=" h-screen">
-          <div className="flex flex-row  gap-16 ml-20 pt-5">
-            <p className="text-[#2838C1] font-semi-bold text-xl">Orders</p>
-            <p className="text-[#2838C1] font-semi-bold text-xl">Buy Again</p>
-          </div>
+      {!loading ? (
+        <div className=" mt-14  h-auto mx-2">
+          <div className=" h-screen">
+            {orders.length > 0 ? (
+              <>
+                <div className="flex flex-row  gap-16 ml-20 pt-5">
+                  <p className="text-black font-bold text-3xl">Orders</p>
+                </div>
 
-          <div className="flex flex-row  gap-2 ml-20 mt-6 w-1/3">
-            <p>21 orders placed in: </p>
-            <select
-              name="category"
-              value={category}
-              onChange={(event) => handleCategoryChange(event.target.value)}
-            >
-              <option id="0">3months</option>
-              <option id="1">6months</option>
-              <option id="1">1yr</option>
-            </select>
-          </div>
+                <div className="flex flex-row text-black gap-2 ml-20 mt-6 w-1/3">
+                  <p>21 orders placed in: </p>
+                  <select
+                    name="category"
+                    value={category}
+                    onChange={(event) =>
+                      handleCategoryChange(event.target.value)
+                    }
+                  >
+                    <option id="0">3months</option>
+                    <option id="1">6months</option>
+                    <option id="1">1yr</option>
+                  </select>
+                </div>
 
-          {orders.map((_) => {
-            <Order />;
-          })}
+                {orders.map((order) => {
+                  return <Order key={order._id} data={order} />;
+                })}
+              </>
+            ) : (
+              <div className="flex flex-col items-center justify-between h-40">
+                <p className="text-black font-bold text-4xl">
+                  You have not placed any orders yet.
+                </p>
+                <button
+                  className="btn btn-outline w-[300px] my-1 text-white bg-darkGreen hover:bg-primaryColor hover:text-black"
+                  onClick={() => navigate("/products")}
+                >
+                  Browse All Products
+                </button>
+              </div>
+            )}
+          </div>
         </div>
-      </div>
+      ) : (
+        <CustomLoader />
+      )}
     </>
   );
 };
